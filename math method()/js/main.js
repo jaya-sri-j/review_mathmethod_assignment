205f0b7a152dc8901cfa74544b8753d617be0b52
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 09:24:54 +0530

    adding math methods.

---
+++ math method()/js/main.js
@@ -0,0 +1,84 @@
+//math.max()
+/*var value=Math.max(10,-4,20,35);            //35
+document.write("first test value:"+value);*/
+
+
+//math.min()
+/*var value=Math.min(10,-4,20,35);            //-4
+document.write(value);*/
+   
+
+//math.round()
+/*var value1=Math.round(0.5);                  //1
+var value2=Math.round(0.3);                    //0
+var value3=Math.round(-0.3);                   //0
+var value4=Math.round(-0.6);                   //-1
+var value5=Math.round(null);                   //0
+var value6=Math.round("string");               //NaN
+document.write(value1);*/
+
+
+//math.fround()
+var value1=Math.fround(10);            //10
+var value2=Math.fround(2.5); 
+document.write("first test value:"+value2);
+
+
+//math.ceil()-max
+//var value1=Math.ceil(6.3);                      //7
+//var value2=Math.ceil(45.20);                    //46
+//var value3=Math.ceil(-45.95);                   //-45
+//var value4=Math.ceil(-45.20);                   //-45
+//document.write(value1);
+
+
+//math.floor()-min
+/*var value1=Math.floor(10.3);                    //10
+var value2=Math.floor(30.9);                    //30
+var value3=Math.floor(-2.9);                    //-3
+var value4=Math.floor(-2.2);                      //-3
+document.write(value1);*/
+
+
+
+//math.random()
+/*var value=Math.random();     
+document.write(value);*/
+
+
+//math.sqrt-square
+/*var value1=Math.sqrt(0.5);                  //0.7071067811865476
+var value2=Math.sqrt(81);                     //9
+var value3=Math.sqrt(13);                     //3.605551275463989
+var value4=Math.sqrt(-4);                     //nan
+document.write(value1);*/
+
+//math.cbrt-square
+//var value1=Math.cbrt(8);                        //2
+//var value2=Math.cbrt(4);                        //1.5874010519681996
+//var value3=Math.cbrt(9);                        //2.080083823051904
+//var value4=Math.cbrt(-4);                         //-1.5874010519681996
+//document.write(value);
+
+
+//math.pow()-power
+/*var value1=Math.pow(7,2);                      //49
+var value2=Math.pow(8,8);                       //16777216
+var value3=Math.pow(-2,2);                      //4
+var value4=Math.pow(0,10);                     //0
+document.write(value1);*/
+
+//math.abs()-absolute
+/*var value1=Math.abs(-8);                      //8
+var value2=Math.abs(null);                    //0
+var value3=Math.abs(20);                      //20
+var value4=Math.abs("string");               //nan
+document.write(value1);*/   
+	 
+//math.acos()-arccosine
+//var value1=Math.acos(-1);                      //3.141592653589793
+//var value2=Math.acos(null);                  //
+//var value3=Math.acos(0);                     //
+// var value4=Math.acos(0);                    //20
+//var value5=Math.acos("string");              //nan
+//document.write(value1);  	
